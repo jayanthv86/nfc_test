10b9b9c46e7a65c34e7a2e498d8a9bd6a3b35b1e
jayanthv86 <jayanthv86@gmail.com>
2016-09-24 19:33:46 -0400

    CSV edit

--- nfc_test.py
+++ nfc_test.py
@@ -2,11 +2,29 @@ import nfc
 import nfc.ndef
 import time
 import sys
+import ast
+import csv
+import json
+connected = False
 
 def on_connect(tag):
     print(tag)
     if tag.ndef:
-        print(tag.ndef.message.pretty())
+        record_1 = tag.ndef.message[0]
+        smartposter = nfc.ndef.SmartPosterRecord(record_1)
+        title = smartposter.title
+	csv_record = title.get('en').encode('utf-8')
+	csv_record = csv_record.replace("u'", "'")
+	print(csv_record)
+        print(type(ast.literal_eval(csv_record)))
+        mydict = ast.literal_eval(csv_record)
+	print(mydict)
+        
+        f = open('mycsvfile.csv', 'w')  # Just use 'w' mode in 3.x
+        w = csv.DictWriter(f, mydict.keys())
+        w.writeheader()
+        w.writerow(mydict)
+
     time.sleep(5)
     return True
 
@@ -26,11 +44,10 @@ def connect_reader(path):
 
 while True:
     try:
-        clf = connect_reader('usb:072F:2200')
+        if not connected:
+            clf = connect_reader('usb:072F:2200')
+            connected = True
     except KeyboardInterrupt:
         sys.exit(-1)
 
-    try:
-        clf.connect(rdwr=rdwr_options)
-    finally:
-        clf.close()
+    clf.connect(rdwr=rdwr_options)
